c7c8b078b4d92d572c7e48c07604da38816c3737
JohnAzizJA <johnazizja10@gmail.com>
2025-04-05 13:33:21 +0200

    Appointment and Medical Record Classes

---
+++ hospital/src/main/java/com/hospital/MedicalRecord.java
@@ -0,0 +1,51 @@
+package com.hospital;
+
+public class MedicalRecord {
+    long recordID;
+    long patientID;
+    String diagnosis;
+    String treatment;
+
+    public MedicalRecord(long recordID, long patientID, String diagnosis, String treatment) {
+        this.recordID = recordID;
+        this.patientID = patientID;
+        this.diagnosis = diagnosis;
+        this.treatment = treatment;
+    }
+
+    public long getRecordID() {
+        return recordID;
+    }
+
+    public void setRecordID(long recordID) {
+        this.recordID = recordID;
+    }
+
+    public long getPatientID() {
+        return patientID;
+    }
+
+    public void setPatientID(long patientID) {
+        this.patientID = patientID;
+    }
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(String diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+
+    public String getTreatment() {
+        return treatment;
+    }
+
+    public void setTreatment(String treatment) {
+        this.treatment = treatment;
+    }
+    
+    public void display() {
+        System.out.println("Patient " + patientID + ": Diagnosis: " + diagnosis + ", Treatment: " + treatment);
+    }
+}
