3a70b92cb7476083781ff12c6aa50eba95635103
Michael-Maged <michaelmaged100@gmail.com>
2025-04-07 18:26:46 +0200

    registration page

---
+++ hospital/src/main/java/com/hospital/controllers/PatientRegistrationController.java
@@ -0,0 +1,31 @@
+package com.hospital.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import com.hospital.Hospital;
+
+@Controller
+public class PatientRegistrationController {
+
+    private Hospital hospital = new Hospital();
+
+    @GetMapping("/register")
+    public String showRegistrationForm() {
+        return "patient-registration";
+    }
+
+    @PostMapping("/register")
+    public String registerPatient(
+            @RequestParam("patientID") int patientID,
+            @RequestParam("name") String name,
+            @RequestParam("age") int age,
+            @RequestParam("gender") String gender,
+            @RequestParam("address") String address,
+            @RequestParam("phoneNumber") String phoneNumber
+    ) {
+        hospital.registerPatient(patientID, name, age, gender, address, phoneNumber);
+        return "patient-login";
+    }
+}
