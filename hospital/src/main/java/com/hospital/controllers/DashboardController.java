187d006969b254b1574033ef3d1341eb023cb163
Michael-Maged <michaelmaged100@gmail.com>
2025-04-08 15:26:04 +0200

    time from string to time

--- hospital/src/main/java/com/hospital/controllers/DashboardController.java
+++ hospital/src/main/java/com/hospital/controllers/DashboardController.java
@@ -22,7 +22,7 @@ public class DashboardController {
 
         if (patient != null) {
             model.addAttribute("patient", patient);
-            model.addAttribute("appointments", patient.getAppointments());
+            model.addAttribute("appointments", patient.FetchUserAppointments());
             return "dashboard";
         } else {
             model.addAttribute("error", "No patient logged in");
@@ -33,16 +33,21 @@ public class DashboardController {
     @PostMapping("/appointments/add")
     public String addAppointment(@RequestParam String type,
                                 @RequestParam Date date,
-                                @RequestParam Time time,
+                                @RequestParam String time,
                                 @RequestParam int docID,
                                 Model model) {
         Patient patient = Session.getInstance().getCurrentPatient();
 
         if (patient != null) {
-            
-            Appointment appointment = new Appointment(hospital.getNextAppointmentId(type),patient.getPatientID(),type,date,time,docID);
-
-            hospital.scheduleAppointment(hospital.getNextAppointmentId(type),patient.getPatientID(),type,date,time,docID); 
+            if (time.length() == 5) {
+                time += ":00";
+            }
+        
+            Time sqlTime = Time.valueOf(time);
+        
+            Appointment appointment = new Appointment(hospital.getNextAppointmentId(type),patient.getPatientID(),type,date,sqlTime,docID);
+            hospital.scheduleAppointment(hospital.getNextAppointmentId(type),patient.getPatientID(),type,date,sqlTime,docID);
+        
             patient.addAppointment(appointment);
             return "redirect:/dashboard";
         } else {
