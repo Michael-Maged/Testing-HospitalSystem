61ba6907b28fd3629efc39657dc18e0399a687db
Farah <22p0183@eng.asu.edu.eg>
2025-04-15 20:02:39 +0200

    Refactor Patient constructor parameter and remove PatientControllerTest class

--- hospital/src/main/java/com/hospital/Patient.java
+++ hospital/src/main/java/com/hospital/Patient.java
@@ -22,7 +22,7 @@ public class Patient {
     public Connection conn = null;
 
     // Constructor
-    public Patient(int patientID, String name, int age, String gender, String address, String phoneNumber) {
+    public Patient(int patientID, String name, int age, String gender, String address, String phoneNumber2) {
         this.patientID = patientID;
         this.name = name;
         this.age = age;
